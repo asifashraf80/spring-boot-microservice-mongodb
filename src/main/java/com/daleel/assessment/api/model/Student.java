fafe3d1f125478a3ee4d42146029321448d0486c
Muhammad Asif Ashraf <asif.ashraf@gmail.com>
2022-11-02 23:28:26 +0400

    Added validation annotation in the model class

--- src/main/java/com/daleel/assessment/api/model/Student.java
+++ src/main/java/com/daleel/assessment/api/model/Student.java
@@ -3,6 +3,9 @@
  */
 package com.daleel.assessment.api.model;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -28,7 +31,16 @@ public class Student {
 	@Id
 	@JsonProperty(access = Access.READ_ONLY)
 	String id;
+	
+	@NotBlank
+	@Size(max = 50)
 	String firstName;
+	
+	@NotBlank
+	@Size(max = 50)
 	String lastName;
+	
+	@NotBlank
+	@Size(max = 30)
 	String department;
 }
